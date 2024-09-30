cdf399bd6993f97ddf88db6a7e8fb44b651bb410
88jonhan <88jonhan@skola.boras.se>
2024-09-30 11:43:10 +0200

    added buttons for navigating between screens

--- App.tsx
+++ App.tsx
@@ -1,33 +1,50 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+	createNativeStackNavigator,
+	NativeStackScreenProps,
+} from "@react-navigation/native-stack";
 
-function HomeScreen() {
+type RootStackParamList = {
+	Home: undefined;
+	Details: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, "Details", "Home">;
+
+function HomeScreen({ navigation }: Props) {
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
 			<Text>Home Screen</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate("Details")}
+			/>
 		</View>
 	);
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }: Props) {
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
 			<Text>Details Screen</Text>
+			<Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+			<Button title="Go back" onPress={() => navigation.goBack()} />
 		</View>
 	);
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
 				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
